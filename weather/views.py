5220ed53a78aa3a8203e3863f753aa1fba6192b0
Your Name <you@example.com>
2025-11-08 14:12:29 -0300

    a

---
+++ weather/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import requests
+
+def home(request):
+    clima = None
+    if 'cidade' in request.GET:
+        cidade = request.GET['cidade']
+        api_key = '76c7584ee4406560c782fda32315a50c'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br&units=metric'
+        resposta = requests.get(url).json()
+
+        if resposta.get('cod') == 200:
+            clima = {
+                'cidade': resposta['name'],
+                'temperatura': resposta['main']['temp'],
+                'descricao': resposta['weather'][0]['description'],
+                'icone': resposta['weather'][0]['icon'],
+            }
+        else:
+            clima = {'erro': 'Cidade não encontrada 😢'}
+    return render(request, 'home.html', {'clima': clima})
