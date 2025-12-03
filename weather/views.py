cef9a688c52df5f6bd1e4a95512aa1ea31840685
Rian <aguiarsantosrian@gmail.com>
2025-12-03 16:31:15 -0300

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -2,56 +2,87 @@ from django.shortcuts import render
 from django.utils import timezone
 import requests
 
-'''---------------------------------------------------------------------------'''
+from django.shortcuts import render
+from django.utils import timezone
+import requests
 
+# ------------------------------------------------------------
+# Serviço responsável por buscar os dados na API
+# ------------------------------------------------------------
+class WeatherService:
+    API_KEY = '76c7584ee4406560c782fda32315a50c'
+    BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+    def buscar_clima(self, cidade):
+        url = f'{self.BASE_URL}?q={cidade}&appid={self.API_KEY}&lang=pt_br&units=metric'
+        resposta = requests.get(url).json()
+        return resposta
+
+
+# ------------------------------------------------------------
+# Classe responsável por resolver qual ícone usar
+# ------------------------------------------------------------
+class IconResolver:
+    ICONES = {
+        'nublado': 'img/cloudy.png',
+        'névoa': 'img/fog.png',
+        'nuvens dispersas': 'img/cloudy.png',
+        'algumas nuvens': 'img/cloudy.png',
+        'pouca neve': 'img/snowy.png',
+        'céu limpo': 'img/sun.png',
+        'chuva leve': 'img/raining.png',
+        'chuva moderada': 'img/raining.png',
+        'trovoadas': 'img/lightingcloud.png',
+    }
+
+    def escolher_icone(self, descricao):
+        for chave, caminho in self.ICONES.items():
+            if chave in descricao:
+                return caminho
+        return ""  # caso nenhum ícone combine
+
+
+# ------------------------------------------------------------
+# Controlador que monta o dicionário final de clima
+# ------------------------------------------------------------
+class WeatherController:
+    def __init__(self):
+        self.service = WeatherService()
+        self.icon_resolver = IconResolver()
+
+    def obter_clima_formatado(self, cidade):
+        resposta = self.service.buscar_clima(cidade)
+        datahora = timezone.localtime(timezone.now())
+
+        if resposta.get('cod') != 200:
+            return {'erro': 'Cidade não encontrada'}
+
+        descricao = resposta['weather'][0]['description']
+        icone = self.icon_resolver.escolher_icone(descricao)
+
+        return {
+            'cidade': resposta['name'],
+            'temperatura': resposta['main']['temp'],
+            'icone': icone,
+            'descricao': descricao.title(),
+            'data': datahora.strftime('%d/%m/%Y'),
+            'hora': datahora.strftime('%H:%M'),
+        }
+
+
+# ------------------------------------------------------------
+# Views
+# ------------------------------------------------------------
 def home(request):
     clima = None
 
     if 'cidade' in request.GET:
         cidade = request.GET['cidade']
-        api_key = '76c7584ee4406560c782fda32315a50c'
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br&units=metric'
-        resposta = requests.get(url).json()
-        datahora = timezone.localtime(timezone.now())
+        controller = WeatherController()
+        clima = controller.obter_clima_formatado(cidade)
 
-        if resposta.get('cod') == 200:
-            descricao = resposta['weather'][0]['description']
-
-            icones = {
-                'nublado': 'img/cloudy.png',
-                'névoa': 'img/fog.png',
-                'nuvens dispersas': 'img/cloudy.png',
-                'algumas nuvens': 'img/cloudy.png',
-                'pouca neve': 'img/snowy.png',
-                'céu limpo': 'img/sun.png',
-                'chuva leve': 'img/raining.png',
-                'chuva moderada': 'img/raining.png',
-                'trovoadas': 'img/lightingcloud.png',
-            }
-
-            icone = ""
-
-            for tempo, caminho in icones.items():
-                if tempo in descricao:
-                    icone = caminho
-                    break
-                
-            descricao_formatada = descricao.title()      
-
-            clima = {
-                'cidade': resposta['name'],
-                'temperatura': resposta['main']['temp'],
-                'icone': icone,
-                'descricao' : descricao_formatada,
-                'data': datahora.strftime('%d/%m/%Y'),
-                'hora': datahora.strftime('%H:%M'),
-            }
-        else:
-            clima = {'erro': 'Cidade não encontrada'}
-    
     return render(request, 'home.html', {'clima': clima})
 
-'''---------------------------------------------------------------------------'''
 
 def index(request):
     return render(request, 'index.html')
