3b27e787c34e8e065632dce209bf5e14a4141713
Rian Aguiar Santos <aguiarsantosrian@gmail.com>
2026-07-05 23:52:24 -0300

    Update settings.py

--- clima/settings.py
+++ clima/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s!+v!)+)r*d2hfks0v%#w@g8cf1g%g1!o$6^epgd(!nop#x$&r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
